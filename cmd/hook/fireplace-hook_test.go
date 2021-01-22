effa8f7ec5c269db2958355227587d25d251967f
dormao <452635801@qq.com>
2021-01-22 20:22:55 +0800

    build arm

--- cmd/hook/fireplace-hook_test.go
+++ cmd/hook/fireplace-hook_test.go
@@ -6,18 +6,18 @@ import (
 
 func TestHook(t *testing.T) {
 	t.Error("nothing to test...")
-	/*
-		log := logrus.New()
-		log.AddHook(NewFireplaceHook(&FireplaceHookConfig{
-			Application:  "test_app",
-			FireplaceURL: "http://localhost:8099",
-		}))
-		log.WithFields(logrus.Fields{
-			"error":  fmt.Errorf("no error"),
-			"status": "ok",
-			"done":   1,
-			"right":  true,
-		}).Info("testInfo")
-	*/
+
+	// log := logrus.New()
+	// log.AddHook(NewFireplaceHook(&FireplaceHookConfig{
+	// 	Application:  "test_app",
+	// 	FireplaceURL: "http://localhost:8999",
+	// }))
+	// log.WithFields(logrus.Fields{
+	// 	"error":   fmt.Errorf("no error"),
+	// 	"status":  "ok",
+	// 	"done":    1,
+	// 	"right":   true,
+	// 	"Unicode": "九",
+	// }).Info("testInfo")
 	// t.Error()
 }
